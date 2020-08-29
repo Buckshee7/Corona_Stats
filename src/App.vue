715df155e9f071d38618d4726221680101b3cecb
Timothy Saudners <timsaunders88@gmail.com>
2020-08-29 15:13:56 +0100

    added getDateArray function to create an array of dates between start and end points

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ import HeaderTitle from '@/components/HeaderTitle.vue'
 import RegionSelect from '@/components/RegionSelect.vue'
 import DisplayResults from '@/components/DisplayResults.vue'
 import { eventBus } from './main.js'
+import moment from 'moment'
 
 export default {
   name: 'app',
@@ -35,14 +36,13 @@ export default {
       // multiple provinces for some countries so need to avoid duplicates
     countries: function(){
       const countries = []
-      if (!this.worldData) {
-        return countries
+      if (this.worldData) {
+        this.worldData.confirmed.locations.forEach((location) => {
+          if (!countries.includes(location.country)){
+            countries.push(location.country)
+          }
+        })
       }
-      this.worldData.confirmed.locations.forEach((location) => {
-        if (!countries.includes(location.country)){
-          countries.push(location.country)
-        }
-      })
       return countries
     },
     dataForDisplay: function(){
@@ -59,13 +59,47 @@ export default {
       .then((resp)=>resp.json())
       .then((data)=>this.worldData = data)
     },
+
+    getDateArray(startDate, endDate){
+      const dates = []
+      const days = endDate.diff(startDate, 'days')
+      for (let date = startDate.clone(); date < endDate; date=date.add(1, 'day')){
+        let newEntry = date.format('M[/]D[/]YY')
+        dates.push([newEntry])
+      }
+      return dates
+    },
+
     makeObjectFromCountry(country){
-      console.log("made it to country")
-      return {location:"Country"} //not implemented
+      const returnData = {location:country, latest:{confirmed:null, deaths:null, recovered:null}, historical:[]}
+
+      ////for each day since records began (22/01/20) create an array with the date as the first item
+      let dates = this.getDateArray(moment('2020-01-20'), moment())
+      console.log(dates)
+
+
+      for (let key in returnData.latest) {
+      //get latest
+        //get locations that match country
+        const countryLocations = this.worldData[key].locations.filter((location)=>{return location.country.toLowerCase() === country.toLowerCase()})
+        //get total of all locations
+        const total = countryLocations.reduce((accumulator, location)=>{return accumulator + location.latest},0)
+        //set to latest data
+        returnData.latest[key] = total
+
+      //get historical
+
+
+      }
+
+      
+      //for each of confirmed,deaths,recovered; match the date and 
+
+      return returnData
     },
     makeObjectFromWorld(){
-      console.log("made it to world")
-      return {location:"World"} //not implemented
+      const returnData = {location:"World", latest:{confirmed:null, deaths:null, recovered:null}, historical:[]}
+      return returnData
     }
   },
 
